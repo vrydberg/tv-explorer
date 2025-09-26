3ab402bb396866914511415cf40f0afec044906b
Victor Rydberg <victorydberg@gmail.com>
2025-09-26 12:34:47 +0200

    delete template views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="max-w-3xl mx-auto bg-gray-400 p-6 border border-blue-500">
+      <!-- Search bar at top -->
+      <div class="mb-8">
+        <!-- <SearchBar v-model:query="query" /> -->
+      </div>
+
+      <!-- Results below -->
+      <!-- <SearchResults :results="results" /> -->
   </main>
 </template>

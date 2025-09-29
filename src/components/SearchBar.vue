54b834ca8401a84aa586e689fa6f92c1864f567b
Victor Rydberg <victorydberg@gmail.com>
2025-09-29 23:26:54 +0200

    add initial structure for explorer search bar vue component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -3,6 +3,18 @@
 </script>
 
 <template>
+
+    <!-- <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required /> -->
+
+    <div class="border-2 border-blue-100 px-6 py-6">
+        <input type="search" class="block mx-auto w-full max-w-3xl px-6 py-6 rounded-full text-md font-medium bg-steel-100" placeholder="Find a movie"></input>
+    </div>
+
+    <div class="flex flex-col border-2 border-green-400 px-6 py-6 items-center">
+        <!-- <h1 class="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-900 dark:text-white">Start Exploring</h1> -->
+        <h1 class="mb-4 text-4xl font-bold leading-none tracking-tight text-steel-100">Start Exploring</h1>
+        <input type="search" class="w-full max-w-3xl px-6 py-6 rounded-full text-md font-medium bg-steel-100" placeholder="Find a movie"></input>
+    </div>
     
     <!-- <form class="max-w-md mx-auto">   
         <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>

643b2f545877cc1688d02f915a6b87a038e95285
Victor Rydberg <victorydberg@gmail.com>
2025-09-30 23:57:26 +0200

    integrate TvShowCard component into search results

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -1,70 +1,20 @@
 <script setup>
+import TvShowCard from './TvShowCard.vue';
+
 
 const props = defineProps(['searchQuery'])
+
+
 
 </script>
 
 <template>
   <div class="flex justify-center px-20 py-6">
 
-    <!-- <div class="grid lg:grid-cols-5 md:grid-cols-3 gap-4 border border-red-600 w-full max-w-7xl"> -->
     <div class="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 w-full max-w-7xl ">
 
-        <div class="w-full">
-            <div class="aspect-[2/3]">
-                <img class="w-full h-full object-cover rounded-lg hover:scale-105 transform transition duration-300" src="@/assets/posters/kitty.jpg" alt="">
-            </div>
 
-            <div class="mt-2">
-                <h3 class="font-semibold text-steel-100">Breaking Bad</h3>
-                <p class="text-sm text-steel-100">Rating: 4</p>
-            </div>
-        </div>
-
-        <div class="w-full">
-            <div class="aspect-[2/3]">
-                <img class="w-full h-full object-cover rounded-lg hover:scale-105 transform transition duration-300" src="@/assets/posters/kitty.jpg" alt="">
-            </div>
-
-            <div class="mt-2">
-                <h3 class="font-semibold text-steel-100">Breaking Bad</h3>
-                <p class="text-sm text-steel-100">Rating: 4</p>
-            </div>
-        </div>
-
-        <div class="w-full">
-            <div class="aspect-[2/3]">
-                <img class="w-full h-full object-cover rounded-lg hover:scale-105 transform transition duration-300" src="@/assets/posters/kitty.jpg" alt="">
-            </div>
-
-            <div class="mt-2">
-                <h3 class="font-semibold text-steel-100">Breaking Bad</h3>
-                <p class="text-sm text-steel-100">Rating: 4</p>
-            </div>
-        </div>
-
-        <div class="w-full">
-            <div class="aspect-[2/3]">
-                <img class="w-full h-full object-cover rounded-lg hover:scale-105 transform transition duration-300" src="@/assets/posters/kitty.jpg" alt="">
-            </div>
-
-            <div class="mt-2">
-                <h3 class="font-semibold text-steel-100">Breaking Bad</h3>
-                <p class="text-sm text-steel-100">Rating: 4</p>
-            </div>
-        </div>
-
-        <div class="w-full">
-            <div class="aspect-[2/3]">
-                <img class="w-full h-full object-cover rounded-lg hover:scale-105 transform transition duration-300" src="@/assets/posters/kitty.jpg" alt="">
-            </div>
-
-            <div class="mt-2">
-                <h3 class="font-semibold text-steel-100">Breaking Bad</h3>
-                <p class="text-sm text-steel-100">Rating: 4</p>
-            </div>
-        </div>
-        
+        <TvShowCard v-for="c in searchQuery.length"></TvShowCard>        
         
     </div>
 

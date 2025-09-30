a2e0f814b31df5b17caf859328231a2b876d146b
Victor Rydberg <victorydberg@gmail.com>
2025-09-30 17:45:08 +0200

    make search input reactive and emit search changes to parent

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,34 +1,29 @@
 <script setup>
+import { ref } from 'vue'
+
+const value = ref('')
 
 </script>
 
 <template>
 
-    <!-- <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required /> -->
-
-    <div class="border-2 border-blue-100 px-6 py-6">
+    <!-- <div class="border-2 border-blue-500 px-6 py-6 mb-4">
         <input type="search" class="block mx-auto w-full max-w-3xl px-6 py-6 rounded-full text-md font-medium bg-steel-100" placeholder="Find a movie"></input>
-    </div>
+    </div> -->
 
     <div class="flex flex-col border-2 border-green-400 px-6 py-6 items-center">
         <!-- <h1 class="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-900 dark:text-white">Start Exploring</h1> -->
         <h1 class="mb-4 text-4xl font-bold leading-none tracking-tight text-steel-100">Start Exploring</h1>
-        <input type="search" class="w-full max-w-3xl px-6 py-6 rounded-full text-md font-medium bg-steel-100" placeholder="Find a movie"></input>
+        <input
+            v-model="value" 
+            type="search"
+            class="w-full max-w-3xl px-6 py-6 rounded-full text-md font-medium bg-steel-100"
+            placeholder="Find a movie"
+            @input="$emit('search-changed', value)">
+        </input>
+    
     </div>
     
-    <!-- <form class="max-w-md mx-auto">   
-        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-        <div class="relative">
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                </svg>
-            </div>
-            <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
-            <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-        </div>
-    </form> -->
-
 </template>
 
 <style></style>

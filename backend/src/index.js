70548c8dad84ed8b254cb557d84d4fe607d038cf
Victor Rydberg <victorydberg@gmail.com>
2025-10-03 19:44:11 +0200

    add app level error handling middlware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,23 @@
 import express from 'express'
-
+import tmdbRoutes from './routes/tmdbRoutes.js'
 const app = express()
-const port = 3030
+const port = 3000
 
 app.get('/', (req, res) => {
     res.send("Hello world!")
 })
 
 app.use('/api/tmdb', tmdbRoutes)
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  const httpStatusCode = err.httpStatusCode ? err.httpStatusCode : 500
+  const errorMessage = err.message ? err.message : 'Something broke!'
+
+  res.status(httpStatusCode).send(errorMessage)
+})
+
 
 app.listen(port, () => {
   console.log(`Web server started: listening to port ${port}`)

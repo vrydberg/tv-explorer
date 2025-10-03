86e582daf2dfddb833b961d1809ad1fe5dd326c3
Victor Rydberg <victorydberg@gmail.com>
2025-10-03 19:45:25 +0200

    define tmdb routes: search tv show

--- backend/src/routes/tmdbRoutes.js
+++ backend/src/routes/tmdbRoutes.js
@@ -1,10 +1,14 @@
 // Route handler for api/tmdb
-
 import express from 'express'
+import { handleSearchTvShow } from '../controllers/tmdbController.js'
 
 const router = express.Router()
 
 // Root path for this route is 'api/tmdb'
 
-router.get('/', )
+// router.get('/', )
+
+router.get('/search/:searchValue', handleSearchTvShow)
+
+export default router;
 

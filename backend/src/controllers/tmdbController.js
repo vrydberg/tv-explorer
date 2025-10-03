3dab73737b3457c5a17d9200316f91041370b63b
Victor Rydberg <victorydberg@gmail.com>
2025-10-03 19:46:04 +0200

    add controller handling logic and validation for tmdb api response

--- backend/src/controllers/tmdbController.js
+++ backend/src/controllers/tmdbController.js
@@ -1,2 +1,29 @@
 // In this file define the logic for requests to the TMDB API
+import { searchTvShow } from '../api/tmdb.js'
+
+export const handleSearchTvShow = async (req, res, next) => {
+
+    try {
+        const searchValue = req.params.searchValue
+        const tvShowData = await searchTvShow(searchValue)
+
+    
+        console.log(`The type of data is : ${typeof(tvShowData)}`)
+        tvShowData.results.sort((a, b) => b.popularity - a.popularity);
+
+        const top10 = tvShowData.results.splice(0, 10)
+
+        for (let index = 0; index < 10; index++) {
+            const element = tvShowData.results[index];
+            console.log(`Original Name: ${element.original_name} ||  Rating: ${element.vote_average}`)
+            
+        }
+
+        res.send('awesome')
+
+    } catch (error) {
+        next(error)
+    }
+}
+
 

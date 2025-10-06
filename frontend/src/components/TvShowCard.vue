e699068399311a1794c7668a9d9463ac65780a36
Victor Rydberg <victorydberg@gmail.com>
2025-10-06 21:36:17 +0200

    define show obj prop and data interpolation

--- frontend/src/components/TvShowCard.vue
+++ frontend/src/components/TvShowCard.vue
@@ -1,14 +1,20 @@
-<script setup></script>
+<script setup>
+    const props = defineProps(['show'])
+
+</script>
 
 <template>
     <div class="w-full">
         <div class="aspect-[2/3]">
-            <img class="w-full h-full object-cover rounded-lg hover:scale-105 transform transition duration-300" src="@/assets/posters/kitty.jpg" alt="">
+            <img 
+                class="w-full h-full object-cover rounded-lg hover:scale-105 transform transition duration-300"
+                :src="`https://image.tmdb.org/t/p/w500/${show.poster}`"
+            >
         </div>
 
         <div class="mt-2">
-            <h3 class="font-semibold text-steel-100">Breaking Bad</h3>
-            <p class="text-sm text-steel-100">Rating: 4</p>
+            <h3 class="font-semibold text-steel-100">{{ show.title }}</h3>
+            <p class="text-sm text-steel-100">Rating: {{ show.rating }}</p>
         </div>
     </div>
 </template>

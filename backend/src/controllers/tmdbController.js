f891948783f51ccf97b7e407be0a7154eb9b548a
Victor Rydberg <victorydberg@gmail.com>
2025-10-06 21:34:06 +0200

    implement logic for filtering and sorting tv show data

--- backend/src/controllers/tmdbController.js
+++ backend/src/controllers/tmdbController.js
@@ -6,20 +6,20 @@ export const handleSearchTvShow = async (req, res, next) => {
     try {
         const searchValue = req.params.searchValue
         const tvShowData = await searchTvShow(searchValue)
+        const processedShows = tvShowData.results.filter(show => show.vote_count >= 100).sort((a, b) => b.popularity - a.popularity);
+        const top10 = processedShows.splice(0, 10)
 
-    
-        console.log(`The type of data is : ${typeof(tvShowData)}`)
-        tvShowData.results.sort((a, b) => b.popularity - a.popularity);
+        console.log(top10)
 
-        const top10 = tvShowData.results.splice(0, 10)
+        const filteredTop10 = top10.map( (show) => ({
+            id: show.id,
+            title: show.name,
+            rating: show.vote_average,
+            first_date: show.first_air_date, 
+            poster: show.poster_path,
+        })) 
 
-        for (let index = 0; index < 10; index++) {
-            const element = tvShowData.results[index];
-            console.log(`Original Name: ${element.original_name} ||  Rating: ${element.vote_average}`)
-            
-        }
-
-        res.send('awesome')
+        res.json(filteredTop10)
 
     } catch (error) {
         next(error)

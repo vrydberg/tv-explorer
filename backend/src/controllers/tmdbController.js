02c6d95fe31c50341540f3600c13f41937f00dcc
Victor Rydberg <victorydberg@gmail.com>
2025-10-08 18:40:00 +0200

    alter tv show fetch to perform parallel requests based on num pages

--- backend/src/controllers/tmdbController.js
+++ backend/src/controllers/tmdbController.js
@@ -5,21 +5,54 @@ export const handleSearchTvShow = async (req, res, next) => {
 
     try {
         const searchValue = req.params.searchValue
-        const tvShowData = await searchTvShow(searchValue)
-        const processedShows = tvShowData.results.filter(show => show.vote_count >= 100).sort((a, b) => b.popularity - a.popularity);
-        const top10 = processedShows.splice(0, 10)
+        const totalPages = 3
+        const pages = []
+         
+        for (let i = 1; i < totalPages + 1; i++) {
+            pages.push(i)
+        }
 
-        console.log(top10)
+        const requests = pages.map((page) => searchTvShow(searchValue, page))
 
-        const filteredTop10 = top10.map( (show) => ({
-            id: show.id,
-            title: show.name,
-            rating: show.vote_average,
-            first_date: show.first_air_date, 
-            poster: show.poster_path,
-        })) 
+        console.log("***************IS THE ISSUE HERE?")
 
-        res.json(filteredTop10)
+        // const tvShowData = await searchTvShow(searchValue)
+        const tvShowData = []
+        // Parallel requests since need to perform multiple get requests for different pages - will help with results relevance
+        // Map the requests since they immediately return a promise object - then can use promise.all that returns a promise when all the subpromises are fulfilled
+        Promise.all(requests).then((tvShowDataPages) => {
+            tvShowDataPages.forEach(page => {
+                // Each results is an array
+                tvShowData.push(page.results)    
+            });
+
+            // tvShowData is an array of arrays [arr, arr, arr] - use flat() to create a copy with all the sub element
+        
+            const processedShows = tvShowData.flat().filter(show => show.vote_count >= 25).sort((a, b) => b.popularity - a.popularity);
+            const top10 = processedShows.splice(0, 10)
+
+            // console.log(top10)
+
+            const filteredTop10 = top10.map( (show) => ({
+                id: show.id,
+                title: show.name,
+                rating: show.vote_average,
+                first_date: show.first_air_date, 
+                poster: show.poster_path,
+            })) 
+            
+            // console.log(filteredTop10)
+            res.json(filteredTop10)
+        })
+
+        
+
+        
+
+        
+        
+        // const processedShows = tvShowData.results.filter(show => show.vote_count >= 1).sort((a, b) => b.popularity - a.popularity);
+        
 
     } catch (error) {
         next(error)

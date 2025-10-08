6ff6e5e375c8a003383bcf2960dbedba877f2d38
Victor Rydberg <victorydberg@gmail.com>
2025-10-08 16:15:06 +0200

    implement debounce logic to emit input changes after 400ms

--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -1,27 +1,41 @@
 <script setup>
 import { ref } from 'vue'
+import { debounce } from '@/util/debounce'
 
-const value = ref('')
+const emit = defineEmits(['search-changed'])
+const searchValue = ref('')
+
+const emitSearch = (searchValue) => {
+    console.log("emittin")
+    emit('search-changed', searchValue)
+}
+
+// Wait 400ms after user types to emit the function and fetch results
+const debouncedSearch = debounce(emitSearch, 400) 
+
+const handleInput = (searchValue) => {
+    console.log("Handling Input")
+    debouncedSearch(searchValue)
+}
+
+// 1. User emits event based on keystroke change
+// 2. This triggers the handleInput event handler function
+// 3. In this handleInput, we initialize the first debounce
 
 </script>
 
 <template>
 
-    <!-- <div class="border-2 border-blue-500 px-6 py-6 mb-4">
-        <input type="search" class="block mx-auto w-full max-w-3xl px-6 py-6 rounded-full text-md font-medium bg-steel-100" placeholder="Find a movie"></input>
-    </div> -->
-
     <div class="flex flex-col px-6 py-6 items-center">
-        <!-- <h1 class="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-900 dark:text-white">Start Exploring</h1> -->
         <h1 class="mb-10 text-4xl font-bold leading-none text-steel-100">Start Exploring</h1>
         <input
-            v-model="value" 
+            v-model="searchValue" 
             type="search"
             class="w-full max-w-3xl px-6 py-6 rounded-full text-md font-medium bg-steel-100"
             placeholder="Find a movie"
-            @input="$emit('search-changed', value)">
+            @input="handleInput(searchValue)">
+            <!-- @input="$emit('search-changed', value)"> -->
         </input>
-    
     </div>
     
 </template>

655ed5eb3a5dcc7580ee9d7d0cc5ba239a7fa179
Victor Rydberg <victorydberg@gmail.com>
2025-10-08 18:40:39 +0200

    update tmdb search tv show api call to include page param

--- backend/src/api/tmdb.js
+++ backend/src/api/tmdb.js
@@ -2,9 +2,11 @@ import { request } from './index.js'
 
 const TMDB_API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTVhODkxY2UwMWI3OGExZGI4NDNiZDg3NGRmYTM4ZCIsIm5iZiI6MTc1OTQwOTM0Mi4zNjQsInN1YiI6IjY4ZGU3NGJlOWJmZGFhZGRiOTk5MWEyZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.47HvFNBpun-3pS4wTrEQJdAtw5k4jfQuk_iBtihMO2w'
 
-export const searchTvShow= async (searchValue) => {
+export const searchTvShow= async (searchValue, page) => {
     
-    const url = `https://api.themoviedb.org/3/search/tv?query=${searchValue}`
+    const url = `https://api.themoviedb.org/3/search/tv?query=${searchValue}&page=${page}`
+
+    console.log(url)
     
     const config = {
         method: 'GET',
@@ -15,7 +17,7 @@ export const searchTvShow= async (searchValue) => {
     }
 
     const tvShowData = await request(url, config)
-
+    
     return tvShowData
 }
 

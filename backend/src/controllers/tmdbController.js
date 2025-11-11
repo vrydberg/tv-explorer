f08977f254887f3f3849964217f1c6306f843c25
Victor Rydberg <victorydberg@gmail.com>
2025-11-11 22:57:06 +0100

    implement request logic for retrieving trending shows

--- backend/src/controllers/tmdbController.js
+++ backend/src/controllers/tmdbController.js
@@ -1,5 +1,5 @@
 // In this file define the logic for requests to the TMDB API
-import { searchTvShow } from '../api/tmdb.js'
+import { searchTvShow, trendingTvShow } from '../api/tmdb.js'
 
 export const handleSearchTvShow = async (req, res, next) => {
 
@@ -13,8 +13,6 @@ export const handleSearchTvShow = async (req, res, next) => {
         }
 
         const requests = pages.map((page) => searchTvShow(searchValue, page))
-
-        console.log("***************IS THE ISSUE HERE?")
 
         // const tvShowData = await searchTvShow(searchValue)
         const tvShowData = []
@@ -31,7 +29,7 @@ export const handleSearchTvShow = async (req, res, next) => {
             const processedShows = tvShowData.flat().filter(show => show.vote_count >= 25).sort((a, b) => b.popularity - a.popularity);
             const top10 = processedShows.splice(0, 10)
 
-            // console.log(top10)
+            console.log(top10)
 
             const filteredTop10 = top10.map( (show) => ({
                 id: show.id,
@@ -52,6 +50,47 @@ export const handleSearchTvShow = async (req, res, next) => {
         
         
         // const processedShows = tvShowData.results.filter(show => show.vote_count >= 1).sort((a, b) => b.popularity - a.popularity);
+        
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const handleTrendingTvShow = async (req, res, next) => {
+    try {
+        const totalPages = 3
+        const pages = []
+         
+        for (let i = 1; i < totalPages + 1; i++) {
+            pages.push(i)
+        }
+
+        const requests = pages.map((page) => trendingTvShow(page))
+
+        
+        const tvShowData = []
+        
+        Promise.all(requests).then((tvShowDataPages) => {
+            tvShowDataPages.forEach(page => {
+                // Each results is an array
+                tvShowData.push(page.results)    
+            });
+        
+            const processedShows = tvShowData.flat().filter(show => show.vote_count >= 25).sort((a, b) => b.popularity - a.popularity);
+            const top20 = processedShows.splice(0, 20)
+
+            const filteredTop20 = top20.map( (show) => ({
+                id: show.id,
+                title: show.name,
+                rating: show.vote_average,
+                first_date: show.first_air_date, 
+                poster: show.poster_path,
+            })) 
+            
+            res.json(filteredTop20)
+        })
+
         
 
     } catch (error) {

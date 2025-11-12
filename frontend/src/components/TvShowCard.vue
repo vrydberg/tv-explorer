bd875c414bade91b30fba580fbf8d0993d8dbef0
Victor Rydberg <victorydberg@gmail.com>
2025-11-12 17:36:48 +0100

    update tv show card content

--- frontend/src/components/TvShowCard.vue
+++ frontend/src/components/TvShowCard.vue
@@ -7,15 +7,16 @@
     <div class="w-full">
         <div class="aspect-[2/3]">
             <img 
-                class="w-full h-full object-cover rounded-lg hover:scale-105 transform transition duration-300"
+                class="w-full h-full object-cover rounded-md cursor-pointer hover:border-3 hover:border-indigo-600"
                 :src="`https://image.tmdb.org/t/p/w500/${show.poster}`"
             >
         </div>
 
-        <div class="mt-2">
-            <h3 class="font-semibold text-steel-100">{{ show.title }}</h3>
-            <p class="text-sm text-steel-100">Rating: {{ show.rating }}</p>
-        </div>
+        <!-- <div class="flex flex-col justify-between mt-2 items-center"> -->
+            <!-- <h3 class="truncate font-medium text-sm text-steel-100">{{ show.title }}</h3> -->
+            <!-- <h3 class="mb-4 text-base font-bold leading-none text-steel-100">Trending</h3> -->
+            <!-- <p class="text-sm italic text-steel-100">Rating: {{ show.rating }}</p> -->
+        <!-- </div> -->
     </div>
 </template>
 

50bbb0f6a2c67b36eaea77f59055e028d440de5a
Victor Rydberg <victorydberg@gmail.com>
2025-11-20 18:21:33 +0100

    create checkbox component for filtering

---
+++ app/components/FilterCheckbox.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+import { ref, defineProps } from 'vue';
+
+const props = defineProps(['checkboxLabel'])
+
+</script>
+
+<template>
+
+  <div class="mb-2 flex gap-2">
+    <div class="flex h-5 shrink-0 items-center">
+      <div class="group grid size-4 grid-cols-1">
+        <input
+          id="filter-color-0"
+          type="checkbox"
+          name="color[]"
+          value="white"
+          class="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto" />
+        <svg viewBox="0 0 14 14" fill="none" class="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25">
+          <path d="M3 8L6 11L11 3.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="opacity-0 group-has-checked:opacity-100" />
+          <path d="M3 7H11" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="opacity-0 group-has-indeterminate:opacity-100" />
+        </svg>
+      </div>
+    </div>
+    <label for="filter-color-0" class="text-sm text-balance truncate text-gray-600">{{ checkboxLabel }}</label>
+  </div>
+</template>
+
+<style></style>

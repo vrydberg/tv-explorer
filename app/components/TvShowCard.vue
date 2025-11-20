a30bd605570386e3055931563055331bb5ea27a1
Victor Rydberg <victorydberg@gmail.com>
2025-11-20 18:22:15 +0100

    refactor card component with new rating star component

--- app/components/TvShowCard.vue
+++ app/components/TvShowCard.vue
@@ -1,5 +1,30 @@
 <script setup>
-    const props = defineProps(['show'])
+
+const props = defineProps(['show', 'extraInfo'])
+
+const rating = ref(props.show.rating)
+
+const stars = computed(() => {
+    const displayedRating = rating.value
+    const relativeRating = displayedRating / 2;
+    const mappedStars = Math.round(relativeRating * 2) / 2;
+
+    const fullStars = Math.floor(mappedStars);
+    const halfStars = mappedStars % 1 ? 1 : 0;
+
+    const stars = []    
+    for (let i = 0; i < 5; i++) {
+        if (i < fullStars) {
+            stars.push('full')
+        } else if (i <fullStars + halfStars) {
+            stars.push('half')
+        } else {
+            stars.push('empty')
+        }
+    }
+
+    return stars
+})
 
 </script>
 
@@ -14,11 +39,27 @@
             
         </div>
 
-        <!-- <div class="flex flex-col justify-between mt-2 items-center"> -->
-            <!-- <h3 class="truncate font-medium text-sm text-steel-100">{{ show.title }}</h3> -->
-            <!-- <h3 class="mb-4 text-base font-bold leading-none text-steel-100">Trending</h3> -->
-            <!-- <p class="text-sm italic text-steel-100">Rating: {{ show.rating }}</p> -->
-        <!-- </div> -->
+        <div v-if="extraInfo" 
+            class="flex flex-col mt-2 gap-1 items-start">
+            <h3 class="w-full truncate font-medium text-sm">{{ show.title }}</h3>
+            
+            <div class="w-full flex gap-2">
+                <div class="flex gap-1">
+
+                    <RatingStar
+                        v-for="(star, index) in stars"
+                        :key=index
+                        :type=star
+                        class="h-3 w-3 text-indigo-600"
+                        >
+                    </RatingStar>
+                </div>
+
+                <p class="leading-none text-sm">{{ show.rating.toFixed(1) }}</p>
+
+            </div>
+            
+        </div>
     </div>
 </template>
 

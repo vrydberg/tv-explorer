9a84aaa4aeea7c5473261fda48e40ff091463a8e
Victor Rydberg <victorydberg@gmail.com>
2025-11-28 16:38:55 +0100

    create skeleton card for loading data

---
+++ app/components/SkeletonCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+const props = defineProps(['extraInfo'])
+
+</script>
+
+<template>
+    <div class="w-full">
+        <div class="aspect-2/3">
+            <div 
+              class="w-500px w-full h-full flex items-center justify-center bg-eigengrau-200 rounded-md text-eigengrau-600">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-8 w-8">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+              </svg>
+            </div>            
+        </div>
+
+        <div v-if="extraInfo" 
+            class="flex flex-col mt-2 gap-1 items-start">
+            <div class="w-full rounded-md bg-eigengrau-200 h-2"></div>
+            <div class="w-1/2 h-2 rounded-md bg-eigengrau-200"></div>
+        </div>
+    </div>
+</template>
+
+<style></style>

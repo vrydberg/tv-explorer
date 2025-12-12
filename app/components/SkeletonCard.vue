1d947a30562b895471a63c857ed1f7bfd80f06ac
Victor Rydberg <victorydberg@gmail.com>
2025-12-12 13:12:36 +0100

    refactor card components to be more generic

--- app/components/SkeletonCard.vue
+++ app/components/SkeletonCard.vue
@@ -5,14 +5,11 @@ const props = defineProps(['extraInfo'])
 </script>
 
 <template>
-    <div class="w-full">
-        <div class="aspect-2/3">
-            <div 
-              class="w-500px w-full h-full flex items-center justify-center bg-eigengrau-200 rounded-md text-eigengrau-600">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-8 w-8">
-                <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-              </svg>
-            </div>            
+    <div>
+        <div class="aspect-2/3 w-full flex items-center justify-center bg-eigengrau-200 rounded-md text-eigengrau-600">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-8 w-8">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+            </svg>
         </div>
 
         <div v-if="extraInfo" 
--- app/components/TvShowCard.vue
+++ app/components/TvShowCard.vue
@@ -1,6 +1,8 @@
 <script setup>
 
-const props = defineProps(['show', 'extraInfo', 'inCarousel'])
+import { NuxtLink } from '#components'
+
+const props = defineProps(['show', 'useNuxtLink', 'hoverable'])
 
 const rating = ref(props.show.rating)
 
@@ -28,30 +30,35 @@ const stars = computed(() => {
 
 </script>
 
+
+
 <template>
-  <div class="w-full">
-    <div class="relative aspect-2/3 flex justify-center items-center group">
-      <div v-if="inCarousel">
+  <div>
+    <div 
+      class="relative aspect-2/3 w-full flex items-center"
+      :class="{'group': hoverable}">
+
+      <component
+        :is="useNuxtLink ? NuxtLink : 'div'"
+        v-bind="useNuxtLink && show.id ? { to: `/shows/${show.id}` } : {}"
+        class="block w-full h-full"
+      >
+
         <img 
-          class="w-full h-full object-cover rounded-md shadow-xl/30 cursor-pointer"
+          class="w-full h-full object-cover rounded-md shadow-xl/30"
+          :class="{
+              'cursor-pointer hover:outline-3 hover:outline-indigo-600': hoverable
+          }"
           :src="`https://image.tmdb.org/t/p/w500/${show.poster}`"
+          :alt="show.title"
           draggable="false"    
         >
-      </div>
+      </component>
 
-      <NuxtLink v-else
-        to="/"
-      >
-        <img 
-          class="w-full h-full object-cover rounded-md shadow-xl/30 cursor-pointer hover:outline-3 hover:outline-indigo-600"
-          :src="`https://image.tmdb.org/t/p/w500/${show.poster}`"
-          draggable="false"    
-      >
-      </NuxtLink>
-        
-      <div class="absolute pointer-events-none group-hover:flex hidden items-center justify-center p-3 bg-transparent w-full h-full rounded-md z-10">
-        <div class="bg-eigengrau-900/50 backdrop-blur-lg rounded-md  h-full w-full flex flex-col justify-center items-center gap-3 px-2">
-          <h3 class="font-semibold truncate text-xs text-pretty max-h-1/4 text-center text-white">{{ show.title }}</h3>
+      <div v-if="hoverable && show.rating"
+        class="absolute pointer-events-none group-hover:flex hidden items-center justify-center p-3 bg-transparent w-full h-full rounded-md z-10">
+        <div class="bg-eigengrau-900/50 backdrop-blur-lg rounded-md h-full w-full flex flex-col justify-center items-center gap-3 px-2">
+          <h3 class="font-semibold truncate text-xs text-pretty max-h-1/4 text-center text-white">{{ show.title || 'Title Unavailable'}}</h3>
           
           <div class="flex gap-1">
             <RatingStar
@@ -59,32 +66,16 @@ const stars = computed(() => {
               :key=index
               :type=star
               class="h-3 w-3 text-indigo-600"
-              >
+            >
             </RatingStar>
           </div>
           <p class="leading-none text-xs font-semibold text-white">{{ show.rating.toFixed(1) }}</p>
         </div>
       </div>
     </div>
-
-    <div v-if="!extraInfo" 
-      class="flex flex-col mt-2 gap-1 items-start">
-      <h3 class="w-full truncate font-medium text-sm">{{ show.title }}</h3>
-      
-      <div class="w-full flex gap-2 items-center">
-        <div class="flex gap-1">
-          <RatingStar
-              v-for="(star, index) in stars"
-              :key=index
-              :type=star
-              class="h-3 w-3 text-indigo-600"
-              >
-          </RatingStar>
-        </div>
-        <p class="leading-none text-sm">{{ show.rating.toFixed(1) }}</p>
-      </div>
-    </div>
   </div>
+  
+
 </template>
 
 <style></style>

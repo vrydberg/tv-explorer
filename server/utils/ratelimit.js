6406c3537cf257256e44d1056c49b86590402ea9
Victor Rydberg <victorydberg@gmail.com>
2026-01-10 22:47:06 +0100

    create redis rate limit util function

---
+++ server/utils/ratelimit.js
@@ -0,0 +1,13 @@
+import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(10, "10 s"),
+});
+
+export async function validateRateLimit(identifier) {
+  console.log("VALIDATING RATE LIMIT")
+  const result = await ratelimit.limit(identifier)
+  return result // { success, remaining, reset} 
+}

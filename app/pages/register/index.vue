eae1dc4d0581b8cc4129299d062f1f17e5e91b3e
Victor Rydberg <victorydberg@gmail.com>
2026-01-16 17:30:19 +0100

    create routes for login/register and implement pages using form component

---
+++ app/pages/register/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+definePageMeta({
+  layout: 'default',
+})
+</script>
+
+<template>
+  <div class="relative flex-1 flex items-center justify-center px-4">
+    <LoginRegisterForm
+      mode="register"
+    ></LoginRegisterForm>
+  </div>
+</template>
